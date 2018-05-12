5453291a86c5bbf63d2529557dcb853ff74c3d6c
AverinDS <awerin960@yandex.ru>
2018-05-12 15:12:12 +0400

    Change step of predict to 15

--- server/serverML/helpers/prepare_x_predict.py
+++ server/serverML/helpers/prepare_x_predict.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def prepare_x_predict(max_x):
     first_x_for_predict = max_x
-    step = 5
+    step = 15
     count_points_of_predict = 30
 
     x_predict = [i for i in range(first_x_for_predict + step,
